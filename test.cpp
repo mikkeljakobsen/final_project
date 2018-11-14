9e96bf8471c0eb722c8fb01705907cd5700651e9
Mikkel <mikkeljakobsen@hotmail.com>
2018-11-14 10:45:13 +0100

    Init
    Initial commit

---
+++ test.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace cv;
+using namespace std;
+
+ int main()
+ {
+    namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
+
+    int iLowH = 0;
+    int iHighH = 179;
+
+    int iLowS = 0;
+    int iHighS = 255;
+
+    int iLowV = 0;
+    int iHighV = 255;
+
+    //Create trackbars in "Control" window
+    cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+    cvCreateTrackbar("HighH", "Control", &iHighH, 179);
+
+    cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+    cvCreateTrackbar("HighS", "Control", &iHighS, 255);
+
+    cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+    cvCreateTrackbar("HighV", "Control", &iHighV, 255);
+    String folderpath = "../images/marker_color/marker_color_*.png";
+    vector<String> filenames;
+    glob(folderpath, filenames);
+
+
+    while (true)
+    {
+        for (size_t i=0; i<filenames.size(); i++)
+        {
+            Mat imgOriginal = imread(filenames[i], IMREAD_COLOR);
+
+            Mat imgHSV;
+
+            cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+
+            Mat imgThresholded;
+
+            inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+
+            //morphological opening (remove small objects from the foreground)
+            erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+            dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+            //morphological closing (fill small holes in the foreground)
+            dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+            erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+            imshow("Thresholded Image", imgThresholded); //show the thresholded image
+            imshow("Original", imgOriginal); //show the original image
+
+            if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+            {
+                cout << "esc key is pressed by user" << endl;
+                break;
+            }
+        }
+    }
+
+   return 0;
+
+}
